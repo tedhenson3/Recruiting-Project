783b9ef334a72f13761ccd99df0898aa054735b3
Ted Henson <tedhenson3@gmail.com>
2019-02-15 16:30:05 -0500

    prep circuit working scraper grabs all of the urls for each team, in progress

--- prep circuit table scraper.R
+++ prep circuit table scraper.R
@@ -22,14 +22,75 @@ remDr <- rD[["client"]]
 remDr$navigate(prep)
 
 
-table.tag <- 'td , #player-sm-basketball_scoring-table a , .ajax_sort'
+# table.tag <- 'td , #player-sm-basketball_scoring-table a , .ajax_sort'
+# 
+# 
+# player.tag <- '.statPlayer a'
+
+next.tag <- '.paginationElement:nth-child(1) a:nth-child(14)'
+# 
+# roster.tag <- 'roster_players'
+# 
+# 
+# class.name <- 'statPlayer '
+
+team.tag <- '.teamName'
 
 
-player.tag <- '.statPlayer a'
 
-t <- remDr$findElements(using = 'css selector', value = player.tag)
 
-players1=list()
+t <- remDr$findElements(using = 'css selector', value = team.tag)
+
+players1=c()
 players1=sapply(t, function(x){x$getElementText()})
-length(players1)
-View(players1)
+teams <- c(players1)
+
+
+score <- remDr$findElement(using = 'css selector', value = next.tag)
+
+score$clickElement()
+
+
+t <- remDr$findElements(using = 'css selector', value = team.tag)
+
+players1=c()
+players1=sapply(t, function(x){x$getElementText()})
+teams2 <- c(players1)
+
+
+players1=c()
+players1=sapply(t, function(x){x$getElementText()})
+teams <- c(players1)
+
+
+
+
+
+t <- remDr$findElements(using = 'css selector', value = table.tag)
+
+players1=c()
+players1=sapply(t, function(x){x$getElementText()})
+teams2 <- c(players1)
+teams2
+
+
+
+table.tag <- '#player-sm-basketball_player-table a'
+
+team.class <- 'teamName'
+# 
+
+#?remDr$executeScript
+# player.class <- 'even'
+# 
+
+
+odd.tag <- "odd"
+t <- remDr$findElements(using = 'class name', value = team.class)
+
+#t$getElementAttributes('href')
+
+
+odd.players=c()
+odd.players=sapply(t, function(x) {x$getElementAttribute('href')})
+View(odd.players)
